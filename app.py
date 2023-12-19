abb704e6ba4bef6c220ad38a22edb2a9b44f37f5
lievan <0evanli0@gmail.com>
2023-12-19 18:05:51 -0500

    cleanup

--- app.py
+++ app.py
@@ -18,67 +18,62 @@ def get_sheet_url(sheet_id):
 
 SHEET_URL = get_sheet_url("1eo6L93Ck9Uz4EPqcXWxqfKpV2ndP2dfXJ_QapGoxMRs")
 
-@app.route("/")
-def index():
-    r = requests.get(SHEET_URL)
-    if r.status_code != 200:
-        return "Error"
-    decoded = r.content.decode()
-    df = pd.read_csv(StringIO(decoded))
-    chapters = []
-    header_data = {'id': 'intro',
-                    'alignment': 'center',
-                    'hidden': False,
-                    'title': '',
-                    'link': '',
-                    'date':"",
-                    'image': '',
-                    'description': """This web app provides a tour of Tibetan food in the United States.
+INTRO_DESC = [
+    """This web app provides a tour of Tibetan food in the United States.
                                       Scroll slowly to jump from restaurants to restaurants.<br><br>
                                       The restaraunts were found from a variety of sources, primarily inspired by this
                                       <a href=https://www.farandwide.com/s/best-tibetan-restaurants-usa-074f18478b4c41eb>article</a> 
-                                      on America's top Tibetan Restaraunts. Related links are at the bottom of the website.
-                                      <br><br>
-                                      The following restaurants showcase a variety of authentic Tibetan dishes and demonstrate how
+                                      on America's top Tibetan Restaraunts. Related links are at the bottom of the website.""",
+    """The following restaurants showcase a variety of authentic Tibetan dishes and demonstrate how
                                       Tibetan cuisine has adapted to different environments in the states, ranging from
                                       immigrant hubs such as Queens to places where the Tibetan community is not prominent.
                                       Many of the owners and chefs mentioned below have a unique journey in the restaurant industry, 
                                       gaining experience in a variety of different cuisines after moving to the states before opening 
                                       their own establishments. You'll often see the menus of these 
                                       restaurants also including American-Chinese food, Indian food, and Nepalese food and utilizing  concepts
-                                      such as set menus, buffets, and more.
-                                      <br><br>
-                                      If you would like to add a restaraunt to this site, please use 
-                                      this <a href=https://forms.gle/brPyQYc1KKzEvmkR8>Google Form</a>.
-                                      <br><br>
-                                      For more resources on Tibetan culture, feel free to check out this
-                                      <a href=https://tibetanculture.weai.columbia.edu/>Tibetan Culture</a> by Columbia's Tibetan studies department""",
-                    'location': [-73.963036, 40.807384],
+                                      such as set menus, buffets, and more.""",
+    """If you would like to add a restaraunt to this site, please use 
+                                      this <a href=https://forms.gle/brPyQYc1KKzEvmkR8>Google Form</a>.""",
+    """For more resources on Tibetan culture, feel free to check out this
+                                      <a href=https://tibetanculture.weai.columbia.edu/>Tibetan Culture</a> by Columbia's Tibetan studies department
+    """
+]
+
+FOOTER_LINKS = [
+    """<a href=https://www.farandwide.com/s/best-tibetan-restaurants-usa-074f18478b4c41eb>America's 
+                                        Top Tibetan Restaurants Will Have You Asking for Seconds</a>""",
+    """<a href=https://tibetanculture.weai.columbia.edu/tibetan-cuisine-nyc/>
+                                        Tibetan Cuisine in NYC</a>""",
+    """<a href=https://www.eater.com/search?q=Tibet/>
+                                        Eater Articles on Tibetan Cuisine</a> """
+]
+
+COLUMBIA_LAT_LON = [-73.963036, 40.807384]
+
+@app.route("/")
+def index():
+    r = requests.get(SHEET_URL)
+    if r.status_code != 200:
+        return "There was an error loading restaurant data"
+    decoded = r.content.decode()
+    df = pd.read_csv(StringIO(decoded))
+    chapters = []
+    header_data = {'id': 'intro',
+                    'alignment': 'center',
+                    'hidden': False,
+                    'description': "<br><br>".join(INTRO_DESC),
+                    'location': COLUMBIA_LAT_LON ,
                     'mapAnimation': 'flyTo',
                     'rotateAnimation': False,
-                    'callback': ''
                 }
     footer_data = {'id': 'conclusion',
                     'alignment': 'center',
                     'hidden': False,
-                    'title': '',
-                    'link': '',
                     'date':"Related links",
-                    'image': '',
-                    'description': """
-                                      <a href=https://www.farandwide.com/s/best-tibetan-restaurants-usa-074f18478b4c41eb>America's 
-                                        Top Tibetan Restaurants Will Have You Asking for Seconds</a> 
-                                        <br><br>
-                                      <a href=https://tibetanculture.weai.columbia.edu/tibetan-cuisine-nyc/>
-                                        Tibetan Cuisine in NYC</a> 
-                                        <br><br>
-                                        <a href=https://www.eater.com/search?q=Tibet/>
-                                        Eater Articles on Tibetan Cuisine</a> 
-                                    """,
-                    'location': [-73.9626, 40.8075],
+                    'description': "<br><br>".join(FOOTER_LINKS),
+                    'location': COLUMBIA_LAT_LON,
                     'mapAnimation': 'flyTo',
                     'rotateAnimation': False,
-                    'callback': ''
                 }
     chapters.append(header_data)
     for i, row in df.iterrows():
@@ -93,8 +88,7 @@ def index():
             'description': row['description'].replace('"', '').strip().replace("\n", "<br>"),
             'location': [float(row['long']), float(row['lat'])],
             'mapAnimation': 'flyTo',
-            'rotateAnimation': False,
-            'callback': ''}
+            'rotateAnimation': False}
         chapters.append(rest_data)
     chapters.append(footer_data)
     return render_template('index.html', chapters=chapters, MAPBOX_KEY =MAPBOX_KEY )
